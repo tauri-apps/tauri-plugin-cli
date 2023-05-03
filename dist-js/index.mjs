79c1661d81aeb877ea6c1b422bd996edad2af5ba
FabianLars <fabianlars@fabianlars.de>
2023-05-03 07:14:18 +0000

    Run formatter on new plugins
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/4869274987
    
    Co-authored-by: FabianLars <FabianLars@users.noreply.github.com>

--- dist-js/index.mjs
+++ dist-js/index.mjs
@@ -23,7 +23,7 @@ import { invoke } from '@tauri-apps/api/tauri';
  * @since 1.0.0
  */
 async function getMatches() {
-    return await invoke('plugin:cli|cli_matches');
+    return await invoke("plugin:cli|cli_matches");
 }
 
 export { getMatches };
